5ecc06ad6a4bccc9638b77a33b741b1a6a80492c
pmohan17 <43380900+pmohan17@users.noreply.github.com>
2019-04-29 20:55:15 -0400

    Update database.php

--- books/database.php
+++ books/database.php
@@ -2,7 +2,7 @@
 $server   = "localhost";
 $database = "priyamoh_books";
 $username = "priyamoh_books";
-$password = "Pm051698!";
+$password = "*****";
 $conn = mysqli_connect($server, $username, $password, $database);
 if (!$conn) {
     echo "Error: Unable to connect to database. ";
